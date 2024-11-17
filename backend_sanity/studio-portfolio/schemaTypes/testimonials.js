c17cbcaeb636ddf3d20a3b994da653cd70e08b6a
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-17 15:51:03 -0500

    initialized the backend using sanity and created a database for users to upload data

---
+++ backend_sanity/studio-portfolio/schemaTypes/testimonials.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'testimonials',
+  title: 'Testimonials',
+  type: 'document',
+  fields: [
+    {name: 'name', title: 'Name', type: 'string'},
+    {name: 'company', title: 'Company', type: 'string'},
+    {
+      name: 'imageURL',
+      title: 'ImgURL',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {name: 'feedback', title: 'Feedback', type: 'string'},
+  ],
+}
