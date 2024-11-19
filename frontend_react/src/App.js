dde2f33088a3b87a7e66e56fd577d5a6d489d01b
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-18 19:42:35 -0500

    worked on the nav bar with scss and jsx while also troubleshooting some npm errors

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 
+import "./App.scss";
+
 const App = () => {
   return (
     <div className="app">
