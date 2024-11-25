5f9517c2640c1326dd69a40f645a329d4b6fb8de
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-25 18:32:25 -0500

    created the first part of the about page with a map of different abouts

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 import "./About.scss";
 
+const abouts = [
+  { title: "Web dev", description: "I am a good web dev", imgUrl: "" },
+  { title: "Web design", description: "I am a good design dev", imgUrl: "" },
+  { title: "Backend", description: "I am a good backend dev", imgUrl: "" },
+  { title: "DevOps", description: "I am a good devops eng", imgUrl: "" },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text">
+        I know that
+        <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((abouts, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={abouts.title + index}
+          >
+            <img src={abouts.imgUrl} alt={abouts.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {abouts.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {abouts.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
