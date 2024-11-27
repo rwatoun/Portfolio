58978e09528e26914199d4b9ef11c98154edb017
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-27 18:03:03 -0500

    created a new H0C called motionwrap which will give the background color to each section and implemented it

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
 import { urlFor, client } from "../../client";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./About.scss";
 
 // populating data with real abouts
@@ -44,4 +44,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, "about");
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
