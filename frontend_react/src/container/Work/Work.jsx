14770da0cbe5a0740362a1d02dbde497118f6e1d
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-26 19:35:38 -0500

    creating the base of the work section by directly implementing the higher order component

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 import "./Work.scss";
 
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  // fetching data from sanity
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
+
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        {" "}
+        My creative <span>Portfolio</span> <br /> Section{" "}
+      </h2>
+      <div className="app__work-filter">
+        {["UI/UX", "Web App", "Mobile App", "React Js", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text $(activeFilter === item ? 'item-active : '')`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      ></motion.div>
+    </>
+  );
 };
 
-export default Work;
+export default AppWrap(Work, "work");
