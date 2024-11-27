147399b5c660aaeb803e0a66793506adc5a3fb49
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-27 17:44:26 -0500

    worked on the configuration of the skills jsx file due to errors when running the app

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import ReactTooltip from "react-tooltip";
+import { Tooltip } from "react-tooltip";
 import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
-  const [experience, setExperience] = useState([]);
+  const [experiences, setExperience] = useState([]);
   const [skills, setSkills] = useState([]);
 
   // fetching data from sanity
@@ -26,7 +26,7 @@ const Skills = () => {
       <h2 className="head-text">Skills and Experience</h2>
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill) => {
+          {skills.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
@@ -40,9 +40,43 @@ const Skills = () => {
                 <img src={urlFor(skill.icon)} alt={skill.name} />
                 <p className="p-text">{skill.name}</p>
               </div>
-            </motion.div>;
-          })}
+            </motion.div>
+          ))}
         </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <Tooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </Tooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
