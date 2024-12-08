529c3d090db36b9255a90fd509caaf01ef4d6a37
Marwa Hammani <hammanimarwa0@gmail.com>
2024-12-08 16:22:20 -0500

    added the github icon which was missing

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
 
 const SocialMedia = () => {
   return (
@@ -11,6 +12,15 @@ const SocialMedia = () => {
           rel="noopener noreferrer"
         >
           <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://github.com/rwatoun"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiFillGithub />
         </a>
       </div>
     </div>
